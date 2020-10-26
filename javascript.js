93fd9b0b5691d93028caa668bd34ea91abb4f399
Mona Lisa <dharmikpatel7@hotmail.com>
2020-10-25 22:00:22 -0400

    .

--- javascript.js
+++ javascript.js
@@ -1,14 +1,13 @@
-const form = document.querySelector(".container");
+document.getElementById("myBtn").addEventListener("click", displayDate);
 
-form.addEventListener("search", e => {
-    e.preventDefault();
-    const inputVal = input.value;
-});
-
+function searchResult() {
+    console.log("button clicked");
+}
 
 
 
-const apiKey = "fe04c9ca681cb4380de909d2c6827a35";
-const inputVal = input.value;
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=$fe04c9ca681cb4380de909d2c6827a35&units=metric`;
+// const apiKey = "fe04c9ca681cb4380de909d2c6827a35";
+// const inputVal = input.value;
+
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=$fe04c9ca681cb4380de909d2c6827a35&units=metric`;
